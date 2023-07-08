9b90954cccc8f08e032033b79bcd8806d4e3b09a
sowmik-sec <sowmik.sec@gmail.com>
2023-07-08 12:05:31 +0600

    navbar created

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="">
+    <div className="container">
       <Navbar />
       <Main />
     </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,17 @@
 import React from "react";
+import ReactIcon from "./react.png";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar goes here</h1>
+    <div className="nav-bar">
+      <div className="nav-logo">
+        <img src={ReactIcon} alt="" width={40} height={40} />
+        <h3>ReactFacts</h3>
+      </div>
+      <div>
+        <h3 className="course">React Course - Project 1</h3>
+      </div>
     </div>
   );
 };
